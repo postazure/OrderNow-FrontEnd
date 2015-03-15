eea8ffa879023fd601261d96b40963fca4175175
Max Gordon <gordonmaxc@gmail.com>
2015-03-14 20:59:14 -0700

    added detail template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,19 +11,7 @@ function setList(restaurants) {
 
 function setDetails(restaurant) {
   console.log(restaurant)
-  var html = detailsTemplate({
-    name: restaurant.name,
-    phone_number: restaurant.phone_number,
-    logo_url: restaurant.logo_url,
-    source_name: restaurant.source_name,
-    source_url: restaurant.source_url,
-    tags: restaurant.tags,
-    rating: restaurant.yelp_info.rating,
-    rating_image_url: restaurant.yelp_info.rating_image_url,
-    review_count: restaurant.yelp_info.review_count,
-    snippet_image_url: restaurant.yelp_info.snippet_image_url,
-    snippet_text: restaurant.yelp_info.snippet_text
-  });
+  var html = detailsTemplate({restaurant:restaurant});
   $('.content.active').html(html)
   // $('.content.active').html("<h1>Something is missing here</h1>")
 }
