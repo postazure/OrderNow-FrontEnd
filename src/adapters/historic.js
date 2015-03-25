ffd05df8cc76538c3ee5aaeb50f3177740c2cb11
Max Gordon <gordonmaxc@gmail.com>
2015-03-25 16:58:20 -0700

    removed redundent code

--- src/adapters/historic.js
+++ src/adapters/historic.js
@@ -1,18 +1,3 @@
-// var historicAdapter = {
-//   url: "https://delivery-now-sf.herokuapp.com/historic/",
-//   // url: "http://localhost:3000/historic/",
-//   report: function (restaurant) {
-//     var url = this.url + restaurant.id;
-//     $.ajax({
-//       method: "post",
-//       url: url,
-//       data: {"wait": restaurant.delivery_time}
-//     });
-//   }
-// }
-
-
-// OO Version
 var HistoricAdapter = function () {
   this.url = "https://delivery-now-sf.herokuapp.com/historic/";
   this.report = function (restaurant) {
